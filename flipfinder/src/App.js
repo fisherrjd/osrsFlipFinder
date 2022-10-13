87d14d6dbf44245e48ea249411511ff38ead3ee8
jade <fisherrjd@gmail.com>
2022-10-13 04:56:49 +0000

    component beginnings

--- flipfinder/src/App.js
+++ flipfinder/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/getItem.js'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        16ind is smol pp energy
+        <getItem></getItem>
       </header>
     </div>
   );
---
+++ flipfinder/src/components/getItem.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+class getItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/item/4151")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.item_id}>
+              {item.item_high_price} {item.item_high_time}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
