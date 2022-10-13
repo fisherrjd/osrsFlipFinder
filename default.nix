650278fdb7efdfc6a2d1726b5aa26f767ff990ac
jade <fisherrjd@gmail.com>
2022-10-13 20:40:45 +0000

    url forwarding

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
 { jacobi ? import
     (
       fetchTarball {
-        name = "jpetrucciani-2022-09-26";
-        url = "https://github.com/jpetrucciani/nix/archive/26fa1fff87b005c5674d9093dd01db36bc316863.tar.gz";
-        sha256 = "13jafqvxn46aazkvwxw74bpaj8nwrqx8is245bzsrwrjhr13zd7m";
+        name = "jpetrucciani-2022-10-12";
+        url = "https://github.com/jpetrucciani/nix/archive/d1c781aada62bdbd91fcad427759167ef1c9b59b.tar.gz";
+        sha256 = "0j3sy7qpxyaqmqqb0iywlw0ism5qwy2bf73l125rlv9vxxrbjdag";
       }
     )
     { }
 }:
 let
-  inherit (jacobi.hax) ifIsLinux ifIsDarwin;
-
   name = "osrsFlipFinder";
   tools = with jacobi; {
+    caddy = [
+      zaddy
+    ];
     cli = [
       jq
       nixpkgs-fmt
     ];
     python = [
-      #      prospector-177
       (python310.withPackages (p: with p; [
         requests
         mypy
@@ -27,7 +27,6 @@ let
         types-requests
       ]))
     ];
-
     react = [
       nodePackages.create-react-app
       nodejs
