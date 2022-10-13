91b1f943e5f8e6e177439c7d04d2edd2c2773ae6
jacobi petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2022-10-13 16:45:24 -0500

    update readme and add much better dev experience (#1)

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-Add documentation
+# osrsflipfinder
 
+[![python style: black](https://img.shields.io/badge/python%20style-black-000000.svg?style=flat-square)](https://github.com/ambv/black)
+[![Python 3.10+ supported](https://img.shields.io/badge/python-3.10+-blue.svg)](https://www.python.org/downloads/release/python-3100/)
+[![uses nix](https://img.shields.io/badge/uses-nix-%237EBAE4)](https://nixos.org/)
 
-to start api:            navigate to flipfinder/src/python --> uvicorn main:app --reload
-to start react:          navigate to flipFinder folder --> npm start
+this is a web app that does <insert good stuff here>
+
+## Running
+
+### the good way
+
+```bash
+# run everything we need!
+run
+```
+
+### the individual way
+
+```bash
+# run fastapi server
+run-backend
+
+# run react webpack server in development mode
+run-frontend
+
+# run caddy reverse proxy
+run-caddy
+```
+
+## TODO: Add more documentation
--- default.nix
+++ default.nix
@@ -10,29 +10,67 @@
 }:
 let
   name = "osrsFlipFinder";
-  tools = with jacobi; {
-    caddy = [
-      zaddy
-    ];
-    cli = [
-      jq
-      nixpkgs-fmt
-    ];
-    python = [
-      (python310.withPackages (p: with p; [
+  tools = with jacobi;
+    let
+      python = python310.withPackages (p: with p; [
         requests
         mypy
         fastapi
         uvicorn
         types-requests
-      ]))
-    ];
-    react = [
-      nodePackages.create-react-app
-      nodejs
-      yarn
-    ];
-  };
+      ]);
+      run-caddy = pog {
+        name = "run-caddy";
+        script = ''
+          ${zaddy}/bin/caddy run --config ./conf/Caddyfile --watch "$@"
+        '';
+      };
+      run-frontend = pog {
+        name = "run-frontend";
+        script = ''
+          cd ./flipfinder/ || exit
+          BROWSER=none ${nodejs}/bin/npm start
+        '';
+      };
+      run-backend = pog {
+        name = "run-backend";
+        script = ''
+          cd ./flipfinder/src/python/ || exit
+          ${python}/bin/uvicorn main:app --reload
+        '';
+      };
+      run = pog {
+        name = "run";
+        script = ''
+          ${concurrently}/bin/concurrently \
+            --names "caddy,react,fastapi" \
+            --prefix-colors "cyan,blue,green" \
+            "run-caddy" \
+            "run-frontend" \
+            "run-backend"
+        '';
+      };
+    in
+    {
+      cli = [
+        jq
+        nixpkgs-fmt
+      ];
+      python = [
+        python
+      ];
+      react = [
+        nodePackages.create-react-app
+        nodejs
+        yarn
+      ];
+      scripts = [
+        run-backend
+        run-caddy
+        run-frontend
+        run
+      ];
+    };
 
   env = jacobi.enviro {
     inherit name tools;
