98a398baba58e434e8af4a23ecc56f8d30f01f14
Jade (Robert) Fisher <92651075+fisherrjd@users.noreply.github.com>
2022-11-01 13:59:29 -0600

    remove class name and fice placeholder home page (#10)

--- flipfinder/src/components/PlaceholderHomepage.jsx
+++ flipfinder/src/components/PlaceholderHomepage.jsx
@@ -4,18 +4,19 @@ import {Link} from 'react-router-dom';
 class PlaceholderHome extends Component {
   constructor(props) {
     super(props);
-    this.state = {items: []};
+    this.state = {
+      items: [],
+    };
   }
   componentDidMount() {
     axios.get(`http://${window.location.host}/api/margin`).then((response) => {
       this.setState({items: response.data.items});
-      console.log(this.state.items);
     });
   }
   render() {
     const {items} = this.state;
     return (
-      <table className="rwd-table">
+      <table>
         <tbody>
           <tr>
             <th>Name</th>
@@ -24,9 +25,9 @@ class PlaceholderHome extends Component {
           {items.map((item) => (
             <tr key={item.id}>
               <td>
-                <Link to={`/itemdetails/${item.id}`}>{item.name}</Link>
+                <Link to={`/itemdetails/${item.id}`}>{item.Name}</Link>
               </td>
-              <td>{item.margin}</td>
+              <td>{item.Margin}</td>
             </tr>
           ))}
         </tbody>
