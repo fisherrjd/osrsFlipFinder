3315b7889032af984c0eab4d236a71eb646be334
fisherrjd <fisherrjd@gmail.com>
2022-11-01 17:52:19 +0000

    missed changes

--- flipfinder/src/index.js
+++ flipfinder/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-=======
 import {BrowserRouter} from 'react-router-dom';
->>>>>>> main
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
