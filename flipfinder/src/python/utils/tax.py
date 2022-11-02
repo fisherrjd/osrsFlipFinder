749d4f3e133c2c6c7d41da058ae3849e980797e6
fisherrjd <fisherrjd@gmail.com>
2022-11-02 00:42:52 +0000

    fix error and some other cleanup

--- flipfinder/src/python/utils/tax.py
+++ flipfinder/src/python/utils/tax.py
@@ -2,6 +2,6 @@ def calc_margin(high: int, low: int) -> int:
     """calculate the margin of an item"""
 
     if high > 500000000:
-        return 5000000
+        return int(high - low - (5000000))
     else:
         return int(high - low - (high * 0.01))
