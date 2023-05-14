c70f5ce096fa045932812bf6d230951e88b7f12e
Jade Fisher <fisherrjd@gmail.com>
2023-05-14 10:13:54 -0600

    beginning database creation

--- flipfinder/src/python/data.py
+++ flipfinder/src/python/data.py
@@ -7,8 +7,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import seaborn.objects as so
 
-df = sns.load_dataset("penguins")
-sns.pairplot(df, hue="species")
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+import seaborn.objects as so
+
+
+# Apply the default theme
+sns.set_theme()
+
+# Load an example dataset
+tips = sns.load_dataset("tips")
+
+# Create a visualization
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
 
 plt.show()
+
 print("test")
