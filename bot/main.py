de2110152231e5c333ad94fb6d4d583dbc6c074e
jade fisher <fisherrjd@gmail.com>
2025-02-13 16:04:41 -0700

    update to main function

--- bot/main.py
+++ bot/main.py
@@ -12,24 +12,24 @@ from collections import namedtuple
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-def query_margin():
+def query_margin(limit):
+    # Connect to SQLite database (or create it if it doesn't exist)
+    conn = sqlite3.connect('bot/data_collection/osrs_prices.db')
+    cursor = conn.cursor()
+    # Define a named tuple for the item structure
+    Item = namedtuple('Item', ['name', 'low', 'low_time', 'high', 'high_time', 'margin'])
+    # Query to fetch the top 5 items with the largest margins
+    cursor.execute('''
+        SELECT Item_name, low, low_time, high, high_time, margin 
+        FROM item_prices 
+        ORDER BY margin DESC 
+        LIMIT ?
+    ''', (limit,))
+    rows = cursor.fetchall()
+    # Convert rows into a list of named tuples
+    return [Item(row[0], row[1], row[2], row[3], row[4], row[5]) for row in rows]
 
 
-# Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('bot/data_collection/osrs_prices.db')
-cursor = conn.cursor()
-# Define a named tuple for the item structure
-Item = namedtuple('Item', ['name', 'low', 'low_time', 'high', 'high_time', 'margin'])
-# Query to fetch the top 5 items with the largest margins
-cursor.execute('''
-    SELECT Item_name, low, , lowTime, high, highTime, margin 
-    FROM item_prices 
-    ORDER BY margin DESC 
-    LIMIT 5
-''')
-rows = cursor.fetchall()
-# Convert rows into a list of named tuples
-items = [Item(row[0], row[1], row[2], row[3], row[4], row[5]) for row in rows]
 
 
 
@@ -48,24 +48,18 @@ async def on_ready():
 # !margin {min margin}
 # !profile - DEFINE STILL - fink docs? 
 
-
-# items = [
-#     # ["name of item", low, high, margin]
-#     ["Wine of zamorak", 800, 798, 2],
-#     ["Maple longbow", 277, 275, 2],
-#     ["Harralander potionas…", 785, 783, 2],
-#     ["Mithril seeds", 945, 943, 2],
-#     ["Grimy tarromin", 419, 417, 2],
-# ]
-
-# Headers
-headers = ["Name", "Insta Buy", "Buy Time", "Insta Sell", "Sell Time", "Margin"]
-
-# Generate the table with the custom format
-table = tabulate(items, headers, tablefmt=thick_line)
-
 @bot.command()
-async def top5(ctx):
+async def top(ctx, limit: int = 5):
+    # Cap the limit at 10
+    if limit > 10:
+        limit = 10
+        await ctx.send("The maximum limit is 10. Showing top 10 items.")
+
+    headers = ["Name", "Insta Buy", "Buy Time", "Insta Sell", "Sell Time", "Margin"]
+    items = query_margin(limit)
+    # Headers
+    # Generate the table with the custom format
+    table = tabulate(items, headers, tablefmt=thick_line)
     formatted_table = f"```{table}```"
     embed = discord.Embed(
     title="Item Table",
@@ -75,7 +69,7 @@ async def top5(ctx):
     await ctx.send(formatted_table)
     
 # Print the table
-print(table)
+# print(table)
 
 
 bot.run(TOKEN)
